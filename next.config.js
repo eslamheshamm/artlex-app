787d96e4310fb35e1328b247cc1f8b7e95575b15
eslamheshamm <esl2mhesham@gmail.com>
2021-01-31 18:25:23 +0200

    localize the blog

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	i18n: {
+		// These are all the locales you want to support
+		locales: ["en", "fr", "ar"],
+		// This is the default locale you want to be used when visiting
+		defaultLocale: "en",
+	},
+};
